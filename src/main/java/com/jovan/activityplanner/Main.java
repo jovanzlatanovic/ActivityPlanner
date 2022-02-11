98e511cc221a57518d270a74d1501b577a01368d
jovanzlatanovic <jovanzlat@gmail.com>
2022-02-11 14:17:06 +0100

    FXML Skeleton

--- src/main/java/com/jovan/activityplanner/Main.java
+++ src/main/java/com/jovan/activityplanner/Main.java
@@ -3,21 +3,26 @@ package com.jovan.activityplanner;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/main.fxml"));
+        Scene mainScene = new Scene(fxmlLoader.load(), 1000, 600);
+
+        primaryStage.setTitle("Activity Planner");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        Application.launch(args);
     }
 }
---
+++ src/main/java/com/jovan/activityplanner/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.jovan.activityplanner.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    public void onNewActivityButtonClick() {
+        System.out.println("Button works!");
+    }
+}
