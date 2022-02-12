b1cddecf811a7ee99a3b6ae3cb01f76f26fb4f7b
jovanzlatanovic <jovanzlat@gmail.com>
2022-02-12 01:24:17 +0100

    Create activity progress

--- src/main/java/com/jovan/activityplanner/controller/CreateActivityController.java
+++ src/main/java/com/jovan/activityplanner/controller/CreateActivityController.java
@@ -1,4 +1,29 @@
 package com.jovan.activityplanner.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class CreateActivityController {
+
+    @FXML private Button createButton;
+    @FXML private Button cancelButton;
+
+    @FXML
+    public void onCreateButtonClick() {
+        System.out.println("works");
+        closeDialog();
+    }
+
+    @FXML
+    public void onCancelButtonClick() {
+        closeDialog();
+    }
+
+    private void closeDialog() {
+       Stage stage = (Stage) cancelButton.getScene().getWindow();
+       stage.close();
+    }
+
 }
--- src/main/java/com/jovan/activityplanner/controller/MainController.java
+++ src/main/java/com/jovan/activityplanner/controller/MainController.java
@@ -1,15 +1,38 @@
 package com.jovan.activityplanner.controller;
 
+import com.jovan.activityplanner.Main;
 import com.jovan.activityplanner.model.Activity;
 import com.jovan.activityplanner.model.RootActivity;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class MainController {
 
     @FXML
-    public void onNewActivityButtonClick() {
-        //TODO: open new dialog
+    public void onNewActivityButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("fxml/create_activity.fxml"));
+        Scene createActivityScene = new Scene(loader.load(), 300, 300);
+
+        Node parentNode = (Node) event.getSource();
+
+        Stage stage = new Stage();
+
+        // TODO: When CSS and unified design is implemented, undecorate the stage:
+        // stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.initOwner(parentNode.getScene().getWindow());
+        stage.setScene(createActivityScene);
+        stage.setTitle("Create new activity");
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.show();
     }
 }
