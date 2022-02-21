2586bd127c1db9697527d18d8d952e351d2794a2
jovanzlatanovic <jovanzlat@gmail.com>
2022-02-21 01:16:04 +0100

    Menubar progress

--- src/main/java/com/jovan/activityplanner/controller/MainController.java
+++ src/main/java/com/jovan/activityplanner/controller/MainController.java
@@ -6,19 +6,27 @@ import com.jovan.activityplanner.model.ApplicationModel;
 import com.jovan.activityplanner.model.command.CreateCommand;
 import com.jovan.activityplanner.model.command.UndoCommand;
 import com.jovan.activityplanner.view.CreateActivityDialog;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 import java.io.IOException;
 
 public class MainController {
 
+    @FXML private MenuBar menuBar;
+
     //TODO: Switch activity for root activity, check activity model comment
     @FXML private ListView<Activity> activityListView;
     private ApplicationModel appModel;
     private ActivityModel model;
+    private UndoCommand undoCommand;
 
     public void initialize() {
         // Get activity and app models
@@ -27,15 +35,30 @@ public class MainController {
         activityListView.setItems(model.getActivityList());
 
         // Setup commands
-        //CreateCommand createCommand = new CreateCommand(appModel, model);
-        //UndoCommand undoCommand = new UndoCommand(appModel, model);
-        //todo: link commands to actions
+        undoCommand = new UndoCommand(appModel, model);
+
+        // Menubar initialization
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(e -> {
+            Platform.exit();
+        });
+
+        MenuItem undoMenuItem = new MenuItem("Undo");
+        exitMenuItem.setOnAction(e -> {
+            undoCommand.execute();
+        });
+
+        fileMenu.getItems().add(exitMenuItem);
+        editMenu.getItems().add(undoMenuItem);
+        menuBar.getMenus().addAll(fileMenu, editMenu);
     }
 
     @FXML
     public void onUndoButtonClick(ActionEvent event) {
-        UndoCommand u = new UndoCommand(appModel, model);
-        u.execute();
+        undoCommand.execute();
     }
 
     @FXML
